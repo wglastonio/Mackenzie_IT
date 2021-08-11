bc6c4adc084e5e756769803302bad2e799be10b0
Wglastonio Leite de Sousa <wglastonio@gmail.com>
2021-08-11 09:21:04 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var myname = "Wglastonio L Sousa";
+var ano = "2021";
+var twitter = "@meutwitter";
+var facebook = "@meufacebook";
+var instagran = "@meuinstagran";
+var linkedin = "@meulinkedin";
+var sexo = "Masculino";
+var polo = "Campinas";
+
+document.write(myname, "     |     ");
+document.write(ano, "     |     ");
+document.write(twitter, "     |     ");
+document.write(facebook, "     |     ");
+document.write(instagran, "     |     ");
+document.write(linkedin, "     |     ");
+document.write(sexo, "     |     ");
+document.write(polo);
